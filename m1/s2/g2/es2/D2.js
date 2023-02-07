48f1788063886446a932c714a64b626c4a22ff1a
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-02-07 16:11:46 +0100

    Esercizio settimana 2 giorno 2

---
+++ m1/s2/g2/es2/D2.js
@@ -0,0 +1,158 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// let dataType = ["number","string","boolean","object"];
+// console.log(dataType);
+
+// console.log(dataType[2]);
+
+
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// const NAME = "Andrea";
+// console.log(NAME);
+
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// let number1 = 12;
+// let number2 = 20;
+// let addiction = number1 + number2;
+
+// console.log(addiction);
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// let x = 12;
+// console.log(x);
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// NAME = "Di Paola";
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// let subtraction = 4 - x;
+// console.log(Math.abs((subtraction)));
+
+// let subtraction2 = x - 4;
+// console.log(subtraction2);
+
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+// let name1 = "john";
+// let name2 = "John";
+
+
+// if(name1 != name2) {
+//   console.log("Le due parole sono differenti!");
+// } else {
+//   console.log("Le due parole sono identiche");
+// }
+
+// let risultato = (name1 != name2) ? "Le due parole sono differenti!" : "Le due parole sono identiche";
+// console.log(risultato);
+
+// let risultato = (name1.toLowerCase != name2.toLowerCase) ? "Le due parole sono differenti!" : "Le due parole sono identiche";
+// console.log(risultato);
+
+// let risultato = (name1 != name2) ? "DIVERSE!" : "identiche!";
+
+// document.getElementById("risposta").innerHTML = `Le due variabili sono ${risultato}`
+
+let eta = 20;
+let color = "pink";
+
+// function possoEntrare () {
+//   if (eta >= 18 && eta <= 100) {
+//     return "Puoi entrare";
+//   } else if (eta < 18) {
+//     return "Aspetta qualche anno";
+//   }
+// }
+
+if (eta < 18) {
+  console.log("sei minorenne");
+} else if (eta >= 18 && eta <= 70) {
+  console.log("sei grande");
+} else {
+  console.log("sei vecchio");
+} 
+
+switch(color) {
+  case "red":
+    console.log("il colore che hai scelto e il rosso");
+    break;
+  case "blue":
+    console.log("il colore che hai scelto e il blu");
+    break;
+  case "yellow":
+    console.log("il colore che hai scelto e il giallo");
+    break;
+  case "green":
+    console.log("il colore che hai scelto e il verde");
+    break;
+  default:
+    console.log("non hai selezionato alcun colore tra quelli disponibili!");
+}
+
+// for(let indice = 1; indice <= 10; indice++) {
+//   console.log(`hai digitato il numero ${indice}`);
+// }
+
+let numeri = [1,5,9,45,789,55];
+// console.log(numeri.length);
+
+for (let indice = 0; indice < numeri.length; indice++) {
+  console.log(numeri[indice]);
+}
+
+for (let indice in numeri) {
+  console.log(numeri[indice]);
+}
+
+// let indice3 = 1;
+
+// while (indice3 < 1) {
+//   console.log(indice3);
+//   indice3++;
+// }
+
+// do { 
+//   console.log(indice3);  //1 e stampa
+//   indice3++;             //2
+//  } while (indice3 < 1);  //2
