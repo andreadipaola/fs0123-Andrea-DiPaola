4878d24c112550a415b7e9f2950f29d287f9ffb5
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-02-10 11:58:47 +0100

    Last commit

--- m1/s2/g5/script.js
+++ m1/s2/g5/script.js
@@ -6,5 +6,12 @@ console.log(myArray.push(6));
 console.log(myArray.pop());
 console.log(result);
 
+// DOMANDA 77 TEST SETTIMANA 2
+
+const arr1 = [0];
+const arr2 = [1,2,3];
+arr1.push(arr2);
+console.log(arr1);
+console.log("last " + arr1.pop() + "length: " + arr1.lenght);
 
 
