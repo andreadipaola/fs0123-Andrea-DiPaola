2619aaa879c6ed2b2875c8ecb1240a56ef5bc948
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-02-10 11:14:44 +0100

    First commit

---
+++ m1/s2/g5/script.js
@@ -0,0 +1,10 @@
+// DOMANDA 17 TEST SETTIMANA 2
+
+const myArray = [1,2,3,4,5];
+const result = myArray.push(6) - myArray.pop();
+console.log(myArray.push(6));
+console.log(myArray.pop());
+console.log(result);
+
+
+
